909e398d009aea27653b53a0a6cd1868b187d6b6
EveRania <ev.r@live.com.ar>
2026-01-30 15:22:44 -0300

    Add ex 5

--- TUP-ap-python-u4/ex4.py
+++ TUP-ap-python-u4/ex4.py
@@ -1,4 +1,4 @@
-# 4) Localiza el error en el siguiente bloque de código. Crea una excepción para evitar que el programa se bloquee y además explica en un mensaje al usuario la causa y/o solución:resultado = 10/0
+# 4 Localiza el error en el siguiente bloque de código. Crea una excepción para evitar que el programa se bloquee y además explica en un mensaje al usuario la causa y/o solución:resultado = 10/0
 
 try : 
     resultado = 10/0
